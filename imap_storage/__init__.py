993321c9c2ccfcb3a61ad1198fd4ddbe738dc7b8
Snake-Soft <info@snake-soft.com>
2019-10-02 18:56:26 +0200

    version

--- imap_storage/__init__.py
+++ imap_storage/__init__.py
@@ -1,7 +1,5 @@
 """only these items should be used from outside"""
 import logging
-from git import Repo
-from pathlib import Path
 from .account import Account, AccountManager
 from .connection.config import Config
 from .storage.email.file import *
@@ -9,8 +7,7 @@ from .storage.email.file import *
 __all__ = ('Account', 'AccountManager', 'NAME', 'VERSION', 'DEBUG')
 
 NAME = 'imap-storage'
-VERSION = sorted([str(tag) for tag in
-                  Repo(str(Path(__file__).parent.parent)).tags])[-1]
+VERSION = '0.2.0'
 DEBUG = False
 
 #logging.basicConfig(
