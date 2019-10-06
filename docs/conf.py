cf1ac7bdc4d320e032344344a03c2b7798086c8b
Snake-Soft <info@snake-soft.com>
2019-10-06 16:27:43 +0200

    added .readthedocs.yml

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,8 @@
-import os
-import sys
-#sys.path.insert(0, os.path.abspath('../imap_storage'))
+#===============================================================================
+# import os
+# import sys
+# sys.path.insert(0, os.path.abspath('../imap_storage'))
+#===============================================================================
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
