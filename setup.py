d7aa8abd6d986c55dc7d4bf06f7bf2fea9017358
Snake-Soft <info@snake-soft.com>
2019-10-12 12:14:25 +0200

    removed py2.7

--- setup.py
+++ setup.py
@@ -28,8 +28,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: System :: Filesystems',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7'
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
@@ -40,7 +38,7 @@ setup(
 
     packages=find_packages(exclude=['imap-storage']),
 
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    python_requires='!=2.*, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
 
     install_requires=['imapclient', 'lxml'],
 
